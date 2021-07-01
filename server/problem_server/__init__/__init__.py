314175350b29379048b25cce3820ae74e7bf9ae8
jun0911 <jun.0911@daum.net>
2021-07-02 02:11:31 +0900

    tensorflow install Error

--- server/problem_server/__init__/__init__.py
+++
@@ -1,3 +0,0 @@
-# __init__.py select import module
-
-__all__ = ['echo']
--- server/problem_server/tensorflow.py
+++ server/problem_server/tensorflow.py
@@ -1,2 +1,4 @@
 import tensorflow as tf
 from tensorflow.keras import datasets, layers, models
+
+(train_images, train_labels), (test_images, test_labels) = datasets.mnist.load_data(); 
