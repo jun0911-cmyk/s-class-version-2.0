0546192fa9a7592fba1a461071f9a5e6d6e35869
jun0911 <jun.0911@daum.net>
2021-07-03 17:12:21 +0900

    cnn tensorflow

--- server/problem_server/problem_learning_server.py
+++ server/problem_server/problem_learning_server.py
@@ -1,36 +1,42 @@
 import tensorflow as tf
 from tensorflow.keras import datasets, layers, models
+import numpy as np 
+import matplotlib.pyplot as plt
 
-(train_images, train_labels), (test_images, test_labels) = datasets.mnist.load_data()
+t1 = [[1, 2, 3], [4, 5, 6]]
+
+(train_images, train_labels), (test_images, test_labels) = datasets.cifar10.load_data()
 print('모델을 로드하였습니다.')
-
-train_images = train_images.reshape((60000, 28, 28, 1))
-test_images = test_images.reshape((10000, 28, 28, 1))
-print('모델 이미지를 설정하였습니다.')
 
 # 픽셀 값을 0~1 사이로 정규화합니다.
 train_images, test_images = train_images / 255.0, test_images / 255.0
 print('픽셀값을 정규화하였습니다.')
 
 model = models.Sequential()
-model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)))
+model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)))
 model.add(layers.MaxPooling2D((2, 2)))
 model.add(layers.Conv2D(64, (3, 3), activation='relu'))
 model.add(layers.MaxPooling2D((2, 2)))
 model.add(layers.Conv2D(64, (3, 3), activation='relu'))
 model.add(layers.Flatten())
 model.add(layers.Dense(64, activation='relu'))
-model.add(layers.Dense(10, activation='softmax'))
-
-model.summary();
-print('모델을 생성하였습니다.')
+model.add(layers.Dense(10))
 
 model.compile(optimizer='adam',
-              loss='sparse_categorical_crossentropy',
-              metrics=['accuracy'])
-model.fit(train_images, train_labels, epochs=15)
-print('CNN 머신러닝 진행중...')
+    loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+    metrics=['accuracy'])
+
+history = model.fit(train_images, train_labels, epochs=10, validation_data=(test_images, test_labels))
+
+model.summary()
+
+plt.plot(history.history['accuracy'], label='accuracy')
+plt.plot(history.history['val_accuracy'], label = 'val_accuracy')
+plt.xlabel('Epoch')
+plt.ylabel('Accuracy')
+plt.ylim([0.5, 1])
+plt.legend(loc='lower right')
 
 test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=2)
 
-print('훈련데이터 훈련성공 정확도 : ', test_acc);
+print(test_acc)
