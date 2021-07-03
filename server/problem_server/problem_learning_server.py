ef541f434ddff54eeb97c3a98b13bfe131f4426c
jun0911 <jun.0911@daum.net>
2021-07-04 01:57:23 +0900

    dataupdate

--- server/problem_server/problem_learning_server.py
+++ server/problem_server/problem_learning_server.py
@@ -6,11 +6,8 @@ import matplotlib.pyplot as plt
 t1 = [[1, 2, 3], [4, 5, 6]]
 
 (train_images, train_labels), (test_images, test_labels) = datasets.cifar10.load_data()
-print('모델을 로드하였습니다.')
 
-# 픽셀 값을 0~1 사이로 정규화합니다.
 train_images, test_images = train_images / 255.0, test_images / 255.0
-print('픽셀값을 정규화하였습니다.')
 
 model = models.Sequential()
 model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)))
@@ -22,13 +19,17 @@ model.add(layers.Flatten())
 model.add(layers.Dense(64, activation='relu'))
 model.add(layers.Dense(10))
 
+model.summary()
+
 model.compile(optimizer='adam',
     loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
     metrics=['accuracy'])
 
 history = model.fit(train_images, train_labels, epochs=10, validation_data=(test_images, test_labels))
 
-model.summary()
+test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=2)
+
+print('종합 어큐러시 : ', test_acc)
 
 plt.plot(history.history['accuracy'], label='accuracy')
 plt.plot(history.history['val_accuracy'], label = 'val_accuracy')
@@ -36,7 +37,4 @@ plt.xlabel('Epoch')
 plt.ylabel('Accuracy')
 plt.ylim([0.5, 1])
 plt.legend(loc='lower right')
-
-test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=2)
-
-print(test_acc)
+plt.show()
