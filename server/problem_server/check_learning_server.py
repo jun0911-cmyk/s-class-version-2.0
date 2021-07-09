bbf97b8a545b1124eb7a452647dbfae05e7579bd
jun0911 <jun.0911@daum.net>
2021-07-10 00:30:30 +0900

    drawing

--- server/problem_server/check_learning_server.py
+++ server/problem_server/check_learning_server.py
@@ -7,11 +7,11 @@ img_width = 180
 
 class_names = ['drowing', 'paper', 'problem_text']
 
-cnn_model_path = 'C:/Users/jun09/OneDrive/Desktop/s-class_system_version/s-class_version-2/server/problem_server/model/1625816850'
+cnn_model_path = 'C:/Users/jun09/OneDrive/Desktop/s-class_system_version/s-class_version-2/server/problem_server/model/1625844412'
 
 load_cnn_model = tf.keras.models.load_model(cnn_model_path)
 
-image_data = 'test1.jpg'
+image_data = 'test7.jpg'
 
 path = 'C:/Users/jun09/OneDrive\desktop/s-class_system_version/s-class_version-2/server/problem_server/test_image/' + image_data
 
--- server/problem_server/problem_learning_server.py
+++ server/problem_server/problem_learning_server.py
@@ -109,18 +109,21 @@ val_acc = history.history['val_accuracy']
 loss = history.history['loss']
 val_loss = history.history['val_loss']
 
-epochs_range = range(epochs)
-
 plt.figure(figsize=(8, 8))
-plt.subplot(1, 2, 1)
-plt.plot(epochs_range, acc, label='Training Accuracy')
-plt.plot(epochs_range, val_acc, label='Validation Accuracy')
+plt.subplot(2, 1, 1)
+plt.plot(acc, label='Training Accuracy')
+plt.plot(val_acc, label='Validation Accuracy')
 plt.legend(loc='lower right')
+plt.ylabel('Accuracy')
+plt.ylim([min(plt.ylim()),1])
 plt.title('Training and Validation Accuracy')
 
-plt.subplot(1, 2, 2)
-plt.plot(epochs_range, loss, label='Training Loss')
-plt.plot(epochs_range, val_loss, label='Validation Loss')
+plt.subplot(2, 1, 2)
+plt.plot(loss, label='Training Loss')
+plt.plot(val_loss, label='Validation Loss')
 plt.legend(loc='upper right')
+plt.ylabel('Cross Entropy')
+plt.ylim([0,1.0])
 plt.title('Training and Validation Loss')
+plt.xlabel('epoch')
 plt.show()
