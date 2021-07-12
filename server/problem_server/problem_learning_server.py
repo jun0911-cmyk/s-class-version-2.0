8ccbea8507b021810f65155d456237d1c6bf5c98
jun0911 <jun.0911@daum.net>
2021-07-12 23:29:30 +0900

    tensorflow size update

--- server/problem_server/problem_learning_server.py
+++ server/problem_server/problem_learning_server.py
@@ -51,8 +51,8 @@ data_augmentation = keras.Sequential(
     [
         layers.experimental.preprocessing.RandomFlip("horizontal", 
         input_shape=(img_height, img_width, 3)),
-        layers.experimental.preprocessing.RandomRotation(0.3),
-        layers.experimental.preprocessing.RandomZoom(0.3),
+        layers.experimental.preprocessing.RandomRotation(0.1),
+        layers.experimental.preprocessing.RandomZoom(0.1),
     ]
 )
 
