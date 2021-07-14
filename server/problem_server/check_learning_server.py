49f97c208bb0f1eb3a6909ff5af30f8858e36162
jun0911 <jun.0911@daum.net>
2021-07-14 11:06:34 +0900

    check function update

--- server/problem_server/check_learning_server.py
+++ server/problem_server/check_learning_server.py
@@ -9,35 +9,35 @@ pytesseract.pytesseract.tesseract_cmd = r'C:/Program Files/Tesseract-OCR/tessera
 img_height = 180
 img_width = 180
 
+image_name = 'test7.jpg'
+
 problem_cnn_class_names = ['drawing', 'paper', 'problem']
 
 cnn_model_path = 'C:/Users/jun09/OneDrive/Desktop/s-class_system_version/s-class_version-2/server/problem_server/model/1626093961'
 
-image_name = 'test9.jpg'
+image_path = 'C:/Users/jun09/OneDrive/desktop/s-class_system_version/s-class_version-2/server/problem_server/test_image/' + image_name
 
-def load_model_image(load_model, image_data):
+def load_model_image(load_model):
     load_cnn_model = tf.keras.models.load_model(load_model)
-    image_name = image_data
-    image_path = 'C:/Users/jun09/OneDrive/desktop/s-class_system_version/s-class_version-2/server/problem_server/test_image/' + image_name
     open_image = Image.open(image_path)
-    return load_cnn_model, image_name, image_path, open_image
+    return load_cnn_model, image_path, open_image
 
-def load_image(model_path, image_name):
-    load_cnn_model, image_name, image_path, open_image =  load_model_image(model_path, image_name)
+def load_image(model_path):
+    load_cnn_model, image_path, open_image =  load_model_image(model_path)
     img = keras.preprocessing.image.load_img(image_path, target_size=(img_height, img_width))
     img_array = keras.preprocessing.image.img_to_array(img)
     img_array = tf.expand_dims(img_array, 0)
     return img_array, open_image, load_cnn_model
 
-def predict_image(model_path, image_name, class_names):
-    img_array, open_image, load_cnn_model = load_image(model_path, image_name)
+def predict_image(model_path, class_names):
+    img_array, open_image, load_cnn_model = load_image(model_path)
     predictions = load_cnn_model.predict(img_array)
     score = tf.nn.softmax(predictions[0])
     accuracy = 100 * np.max(score)
     score_class_name = class_names[np.argmax(score)]
     return accuracy, score_class_name, open_image
 
-accuracy, score_class_name, open_image = predict_image(cnn_model_path, image_name, problem_cnn_class_names)
+accuracy, score_class_name, open_image = predict_image(cnn_model_path, problem_cnn_class_names)
 
 if score_class_name == 'problem' and accuracy > 70.0:
     print('Extracting text...')
