313fcfc7463877e3d9effe7af4f58453937b9ddd
jun0911 <jun.0911@daum.net>
2021-07-20 15:53:20 +0900

    rnn_network_learning

---
+++ server/problem_server/classification_learning_server.py
@@ -0,0 +1,83 @@
+import numpy as np
+import tensorflow_datasets as tfds
+import tensorflow as tf
+import matplotlib.pyplot as plt
+
+tfds.disable_progress_bar()
+
+def plot_graphs(history, metric):
+    plt.plot(history.history[metric])
+    plt.plot(history.history['val_'+metric], '')
+    plt.xlabel("Epochs")
+    plt.ylabel(metric)
+    plt.legend([metric, 'val_'+metric])
+    plt.show()
+
+dataset, info = tfds.load('imdb_reviews', with_info=True,
+                          as_supervised=True)
+train_dataset, test_dataset = dataset['train'], dataset['test']
+
+train_dataset.element_spec
+
+class_name = ['good review', 'bad review']
+
+for example, label in train_dataset.take(1):
+    print('text: ', example.numpy())
+    print('label: ', label.numpy())
+
+BUFFER_SIZE = 10000
+BATCH_SIZE = 64
+
+train_dataset = train_dataset.shuffle(BUFFER_SIZE).batch(BATCH_SIZE).prefetch(tf.data.experimental.AUTOTUNE)
+test_dataset = test_dataset.batch(BATCH_SIZE).prefetch(tf.data.experimental.AUTOTUNE)
+
+for example, label in train_dataset.take(1):
+    print('texts: ', example.numpy()[:3])
+    print()
+    print('labels: ', label.numpy()[:3])
+
+VOCAB_SIZE = 1000
+encoder = tf.keras.layers.experimental.preprocessing.TextVectorization(max_tokens=VOCAB_SIZE)
+encoder.adapt(train_dataset.map(lambda text, label: text))
+
+vocab = np.array(encoder.get_vocabulary())
+vocab[:20]
+
+encoded_example = encoder(example)[:3].numpy()
+encoded_example
+
+for n in range(3):
+    print("Original: ", example[n].numpy())
+    print("Round-trip: ", " ".join(vocab[encoded_example[n]]))
+    print()
+
+model = tf.keras.Sequential([
+    encoder,
+    tf.keras.layers.Embedding(len(encoder.get_vocabulary()), 64, mask_zero=True),
+    tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(64,  return_sequences=True)),
+    tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(32)),
+    tf.keras.layers.Dense(64, activation='relu'),
+    tf.keras.layers.Dropout(0.5),
+    tf.keras.layers.Dense(1)
+])
+
+model.compile(loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
+    optimizer=tf.keras.optimizers.Adam(1e-4),
+    metrics=['accuracy'])
+
+history = model.fit(train_dataset, epochs=10,
+    validation_data=test_dataset,
+    validation_steps=30)
+
+export_path = 'C:/Users/jun09/OneDrive/Desktop/s-class_system_version/s-class_version-2/server/problem_server/model/{}'.format(str('rnn_test'))
+
+model.save(export_path)
+
+print('model saving success path : ' + export_path)
+
+plt.figure(figsize=(16, 6))
+plt.subplot(1, 2, 1)
+plot_graphs(history, 'accuracy')
+plt.subplot(1, 2, 2)
+plot_graphs(history, 'loss')
+plt.show()
