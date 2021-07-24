dc09c8f0e3b27839943cefcef3de42b78b8a74e9
jun0911 <jun.0911@daum.net>
2021-07-25 01:41:51 +0900

    ocr_accuracy update

--- server/problem_server/check_learning_server.py
+++ server/problem_server/check_learning_server.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 import numpy as np
 import pytesseract
+import cv2
 from tensorflow import keras
 from PIL import Image 
 
@@ -9,7 +10,7 @@ pytesseract.pytesseract.tesseract_cmd = r'C:/Program Files/Tesseract-OCR/tessera
 img_height = 180
 img_width = 180
 
-image_name = 'test7.jpg'
+image_name = 'test1.jpg'
 
 problem_cnn_class_names = ['drawing', 'paper', 'problem']
 
@@ -19,30 +20,36 @@ image_path = 'C:/Users/jun09/OneDrive/desktop/s-class_system_version/s-class_ver
 
 def load_model_image(load_model):
     load_cnn_model = tf.keras.models.load_model(load_model)
-    open_image = Image.open(image_path)
-    return load_cnn_model, image_path, open_image
+    return load_cnn_model, image_path
 
 def load_image(model_path):
-    load_cnn_model, image_path, open_image =  load_model_image(model_path)
+    load_cnn_model, image_path =  load_model_image(model_path)
     img = keras.preprocessing.image.load_img(image_path, target_size=(img_height, img_width))
     img_array = keras.preprocessing.image.img_to_array(img)
     img_array = tf.expand_dims(img_array, 0)
-    return img_array, open_image, load_cnn_model
+    return img_array, load_cnn_model
 
 def predict_image(model_path, class_names):
-    img_array, open_image, load_cnn_model = load_image(model_path)
+    img_array, load_cnn_model = load_image(model_path)
     predictions = load_cnn_model.predict(img_array)
     score = tf.nn.softmax(predictions[0])
     accuracy = 100 * np.max(score)
     score_class_name = class_names[np.argmax(score)]
-    return accuracy, score_class_name, open_image
+    return accuracy, score_class_name
 
-accuracy, score_class_name, open_image = predict_image(cnn_model_path, problem_cnn_class_names)
+def ocr_image(image_name):
+    ocr_iamge_name = cv2.imread(image_name)
+    gray_image = cv2.cvtColor(ocr_iamge_name, cv2.COLOR_BGR2GRAY)
+    ocr_predict_image = cv2.threshold(gray_image, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+    problem_text = pytesseract.image_to_string(ocr_predict_image, lang='kor+eng')
+    return problem_text
+
+accuracy, score_class_name = predict_image(cnn_model_path, problem_cnn_class_names)
 
 if score_class_name == 'problem' and accuracy > 70.0:
     print('Extracting text...')
-    problem_text = pytesseract.image_to_string(open_image, lang='kor+eng+equ')
-    print(problem_text)
+    ocr_problem_text = ocr_image(image_path)
+    print(ocr_problem_text)
 else:
     print(
         "Failed to extract text, less than 70% accuracy or not problematic. Measured Results : {}, result accuracy : {:.2f}%"
