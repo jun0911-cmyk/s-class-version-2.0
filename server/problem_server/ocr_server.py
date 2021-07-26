874c1cc39abc503f8e7e4d723318e47c20fd80f8
jun0911 <jun.0911@daum.net>
2021-07-26 23:55:38 +0900

    ML new file created

--- server/problem_server/ocr_server.py
+++ server/problem_server/ocr_server.py
@@ -9,28 +9,32 @@ pytesseract.pytesseract.tesseract_cmd = r'C:/Program Files/Tesseract-OCR/tessera
 img_height = 180
 img_width = 180
 
-image_name = 'test13.jpg'
+image_name = 'test1.jpg'
 
-problem_cnn_class_names = ['drawing', 'paper', 'problem']
+class_names = ['drawing', 'paper', 'problem']
 
-cnn_model_path = 'C:/Users/jun09/OneDrive/Desktop/s-class_system_version/s-class_version-2/server/problem_server/model/cnn_model/1627062415'
+model_path = 'C:/Users/jun09/OneDrive/Desktop/s-class_system_version/s-class_version-2/server/problem_server/model/cnn_model/1627062415'
 
 image_path = 'C:/Users/jun09/OneDrive/desktop/s-class_system_version/s-class_version-2/server/problem_server/test_image/' + image_name
 
-def load_model_image(load_model):
-    load_cnn_model = tf.keras.models.load_model(load_model)
-    return load_cnn_model, image_path
+def load_model():
+    return tf.keras.models.load_model(model_path)
 
-def load_image(model_path):
-    load_cnn_model, image_path =  load_model_image(model_path)
-    img = keras.preprocessing.image.load_img(image_path, target_size=(img_height, img_width))
-    img_array = keras.preprocessing.image.img_to_array(img)
-    img_array = tf.expand_dims(img_array, 0)
-    return img_array, load_cnn_model
+def load_image():
+    return keras.preprocessing.image.load_img(image_path, target_size=(img_height, img_width))
 
-def predict_image(model_path, class_names):
-    img_array, load_cnn_model = load_image(model_path)
-    predictions = load_cnn_model.predict(img_array)
+def image_array():
+    load_img = load_image()
+    img_array = keras.preprocessing.image.img_to_array(load_img)
+    return tf.expand_dims(img_array, 0)
+
+def predict_image():
+    img_array = image_array()
+    model = load_model()
+    return model.predict(img_array)
+
+def accuracy_calculation():
+    predictions = predict_image()
     score = tf.nn.softmax(predictions[0])
     accuracy = 100 * np.max(score)
     score_class_name = class_names[np.argmax(score)]
@@ -42,12 +46,16 @@ def ocr_image(image_name):
     problem_text = pytesseract.image_to_string(ocr_predict_image, config=ocr_config)
     return problem_text
 
-accuracy, score_class_name = predict_image(cnn_model_path, problem_cnn_class_names)
+accuracy, score_class_name = accuracy_calculation()
 
 if score_class_name == 'problem' and accuracy > 70.0:
     print('Extracting text...')
     ocr_problem_text = ocr_image(image_path)
     print(ocr_problem_text)
+elif score_class_name == 'problem' and accuracy < 70.0:
+    print(
+        "The image was not accurately recognized. Please select another image or re-recognize it. Current measured accuracy: {:.2f}".format(accuracy)
+    )
 else:
     print(
         "Failed to extract text, less than 70% accuracy or not problematic. Measured Results : {}, result accuracy : {:.2f}%"
